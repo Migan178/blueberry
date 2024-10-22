3c44cb37522ae8a2ab63f2676f00613a41530f39
Siwoo Jeon <me@migan.co.kr>
2024-10-22 22:30:21 +0900

    feat: Add gambling command
    Start the feature/gambling branch.

---
+++ src/commands/gambling.ts
@@ -0,0 +1,54 @@
+import { ApplyOptions } from '@sapphire/decorators'
+import { Command } from '@sapphire/framework'
+import { ChatInputCommandInteraction } from 'discord.js'
+
+@ApplyOptions<Command.Options>({
+  name: '도박',
+  description: '봇 내 재화로 도박을 해요.',
+  detailedDescription: {
+    usage: '/도박 금액:도박할 금액',
+    examples: ['/도박 금액:1000', '/도박 금액:올인'],
+  },
+  preconditions: ['IsJoined', 'IsBlocked', 'CheckChannel'],
+})
+export default class GamblingCommand extends Command {
+  public registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand(builder =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .addStringOption(option =>
+          option
+            .setName('금액')
+            .setDescription('도박할 금액을 입력해주세요.')
+            .setRequired(true),
+        ),
+    )
+  }
+
+  public async chatInputRun(interaction: ChatInputCommandInteraction) {
+    const userInput = interaction.options.getString('금액', true)
+    const user = await this.container.database.user.findFirst({
+      where: {
+        user_id: interaction.user.id,
+      },
+    })
+    let amount: bigint
+
+    if (!user) return
+    if (userInput === '올인' || userInput === '모두' || userInput === '전부') {
+      amount = user.money
+    } else {
+      amount = BigInt(userInput)
+    }
+
+    if (amount > user.money)
+      return await interaction.reply({
+        ephemeral: true,
+        content: '현재 봇 내 재화가 도박을 건 금약보다 적어요.',
+      })
+
+    // TODO: 확률적으로 도박을 건 돈에서 2 ~ 5배로 돌려주거나, 잃기
+    // TODO: 일정 봇 내 재화이상만 걸 수 있도록 하기
+  }
+}
